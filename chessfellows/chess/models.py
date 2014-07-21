69ec6586cd9ce9c8bda5b9c2f6f76ecd4a43baca
jefrailey <jcaetof@gmail.com>
2014-07-21 11:04:17 -0700

    Add get_file_owner_username() to return a file path for a player's profile picture; add photo attribute to Player() model

--- chessfellows/chess/models.py
+++ chessfellows/chess/models.py
@@ -1,11 +1,20 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
+
+
+def get_file_owner_username(instance, filename):
+    parts = [instance.user.username]
+    parts.append(os.path.basename(filename))
+    path = u"/".join(parts)
+    return path
 
 
 class Match(models.Model):
     white = models.ForeignKey(User, related_name="White")
     black = models.ForeignKey(User, related_name="Black")
     moves = models.TextField()
+    date_played = models.DateTimeField(auto_now=True)
 
 
 class Player(models.Model):
@@ -15,13 +24,22 @@ class Player(models.Model):
     losses = models.PositiveIntegerField(default=0)
     draws = models.PositiveIntegerField(default=0)
     matches = models.ManyToManyField(Match, related_name="Player")
-    opponent_rating = models.PositiveIntegerField(default=0)
+    all_opponents_rating = models.PositiveIntegerField(default=0)
+    image_upload_folder = 'photos/'
+    photo = models.ImageField(upload_to=image_upload_folder,
+                              height_field='height',
+                              width_field='width')
+
+    def update_all_opponents_rating(self, other):
+        self.all_opponents_rating += other.rating
 
     def calc_rating(self):
-        numerator = (self.opponent_rating + 400 * (self.wins - self.losses))
+        numerator = (self.opponents_rating + 400 * (self.wins - self.losses))
         denom = self.wins + self.losses + self.draws
         return numerator // denom
 
     def save(self, *args, **kwargs):
+        opponent = Match.objects.filter()
+        self.update_all_opponents_rating(opponent)
         self.rating = self.calc_rating()
         super(Player, self).save(*args, **kwargs)
