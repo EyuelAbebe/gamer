059bc8ca0bd3f67abee8f24675d50e70b72509b1
jefrailey <jcaetof@gmail.com>
2014-07-21 17:11:42 -0700

    Update chessfellows/settings.py to use django-configurations by adding a Common(Configuration) and a Dev(Common)

--- chessfellows/chessfellows/settings.py
+++ chessfellows/chessfellows/settings.py
@@ -1,96 +1,99 @@
-"""
-Django settings for chessfellows project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from configurations import Configuration
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+class Common(Configuration):
+    u"""
+    Django settings for chessfellows project.
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uw2071e1jt(zlsqoq&9*ikayz74_(rk@e^45cronly8!ot8c7x'
+    For more information on this file, see
+    https://docs.djangoproject.com/en/1.6/topics/settings/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    For the full list of settings and their values, see
+    https://docs.djangoproject.com/en/1.6/ref/settings/
+    """
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'south',
-    'chess',
-    'rest_framework',
-    'registration'
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'chessfellows.urls'
-
-WSGI_APPLICATION = 'chessfellows.wsgi.application'
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    import os
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gamer',
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'uw2071e1jt(zlsqoq&9*ikayz74_(rk@e^45cronly8!ot8c7x'
+
+    ALLOWED_HOSTS = []
+
+
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'south',
+        'chess',
+        'rest_framework',
+        'registration'
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ROOT_URLCONF = 'chessfellows.urls'
+
+    WSGI_APPLICATION = 'chessfellows.wsgi.application'
+
+
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'gamer',
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+    EMAIL_USE_TLS = True
+    with open(os.path.join(BASE_DIR, 'access/email.txt')) as f:
+        EMAIL_HOST = f.readline().strip()
+        EMAIL_PORT = f.readline().strip()
+        EMAIL_HOST_USER = f.readline().strip()
+        EMAIL_HOST_PASSWORD = f.readline().strip()
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_USE_TLS = True
-with open(os.path.join(BASE_DIR, 'access/email.txt')) as f:
-    EMAIL_HOST = f.readline().strip()
-    EMAIL_PORT = f.readline().strip()
-    EMAIL_HOST_USER = f.readline().strip()
-    EMAIL_HOST_PASSWORD = f.readline().strip()
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+    ACCOUNT_ACTIVATION_DAYS = 7
 
-ACCOUNT_ACTIVATION_DAYS = 7
+
+class Dev(Common):
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    STATIC_URL = '/static/'
