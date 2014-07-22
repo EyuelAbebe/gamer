06c95abc56394a054977101a157fa4c77a99d435
jefrailey <jcaetof@gmail.com>
2014-07-22 14:55:52 -0700

    Add engine.py to contain the chess game engine

---
+++ engine.py
@@ -0,0 +1,7 @@
+# Use x, y coords for unit positions
+# (97, 56) ... (104, 56)
+#    ...          ...
+# (97, 49) ... (104, 49)
+#
+# Algebraic notation for a position is:
+# algebraic_pos = chr(x) + chr(y)
