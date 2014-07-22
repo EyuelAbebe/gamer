e2959ec01b25c3f447fdd31608b30f19c2dc3599
jefrailey <jcaetof@gmail.com>
2014-07-22 14:58:51 -0700

    Add _is_pos_on_board() to determine if a position is on the board

--- engine.py
+++ engine.py
@@ -15,3 +15,12 @@ def _coord_to_algebraic(coord):
 def _algebraic_to_coord(algebraic):
     x, y = algebraic[0], algebraic[1]
     return ord(x), ord(y)
+
+
+def _is_pos_on_board(coord):
+    u"""Return True if coordinate is on the board."""
+    x, y = coord
+    if (97 <= x <= 104) and (49 <= y <= 56):
+        return True
+    else:
+        return False
