983cbe47039ea4b031db1be95e22fc514940b06c
jefrailey <jcaetof@gmail.com>
2014-07-21 19:17:20 -0700

    Remove extra ^ in regex expressions in urls.py

--- chessfellows/chessfellows/urls.py
+++ chessfellows/chessfellows/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('chess.urls')),
-    url(r'^', include('registration.backends.default.urls')),
+    url(r'', include('chess.urls')),
+    url(r'', include('registration.backends.default.urls')),
 )
