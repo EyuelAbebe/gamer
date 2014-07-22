c01bd66a916dc8eda6ebadc0eea78c5feb3491f5
jefrailey <jcaetof@gmail.com>
2014-07-22 15:51:26 -0700

    Refactor coord/algebraic converstions from functions to dictionaries

--- engine.py
+++ engine.py
@@ -6,21 +6,21 @@
 # Algebraic notation for a position is:
 # algebraic_pos = chr(x) + chr(y)
 
+# Accept algebraic as input; ultimately return algebraic
+# Use coordinates internally
 
-def _coord_to_algebraic(coord):
-    x, y = coord
-    return chr(x) + chr(y)
-
-
-def _algebraic_to_coord(algebraic):
-    x, y = algebraic[0], algebraic[1]
-    return ord(x), ord(y)
+_coord_to_a1 = dict(
+    [((x, y), chr(x) + chr(y)) for x in xrange(97, 105) for y in xrange(49, 57)]
+    )
+_a1_to_coord = dict(
+    [(chr(x) + chr(y), (x, y)) for x in xrange(97, 105) for y in xrange(49, 57)]
+    )
 
 
 def _is_pos_on_board(coord):
     u"""Return True if coordinate is on the board."""
     x, y = coord
-    if (97 <= x <= 104) and (49 <= y <= 56):
+    if _coord_to_a1.get((x, y), False):
         return True
     else:
         return False
@@ -32,7 +32,7 @@ class Piece(object):
         """Instantiate a piece at a coordinate position."""
         super(Piece, self).__init__()
         if isinstance(pos, str):
-            self.x, self.y = _algebraic_to_coord(pos)
+            self.x, self.y = _a1_to_coord[pos]
         else:
             self.x, self.y = pos
 
@@ -41,7 +41,7 @@ class Piece(object):
             dx, dy = pos
             self.x += dx
             self.y += dy
-            board[_coord_to_algebraic((self.x, self.y))]
+            board[_coord_to_a1[(self.x, self.y)]]
             return board
 
 
@@ -58,6 +58,38 @@ class SimpleUnit(Piece):
             dx, dy = move
             new_pos = (self.x + dx, self.y + dy)
             if _is_pos_on_board(new_pos):
-                if not board[new_pos] or (board[new_pos].color != self.color):
+                alg = _coord_to_a1[new_pos]
+                if not board[alg] or (board[alg].color != self.color):
                     valid_moves.append(new_pos)
         return valid_moves
+
+    def __repr__(self):
+        if self.color == 'black':
+            return 'Sb'
+        else:
+            return 'Sw'
+
+
+def _create_blank_board():
+    board = dict([(_coord_to_a1[(x, y)], None) for x in xrange(97, 105) for y in xrange(49, 57)])
+    return board
+
+
+def _add_simple_units(board=_create_blank_board()):
+    black = [_coord_to_a1[(x, y)] for x in xrange(97, 105) for y in xrange(55, 57)]
+    white = [_coord_to_a1[(x, y)] for x in xrange(97, 105) for y in xrange(49, 51)]
+    for i in black:
+        board[i] = SimpleUnit(i, 'black')
+    for i in white:
+        board[i] = SimpleUnit(i, 'white')
+    return board
+
+start_board = _add_simple_units()
+
+
+def move():
+    """Return a board with the piece moved.
+
+    Accepts algebraic notation.
+    """
+    pass
