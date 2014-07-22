20503ab60e4d2fc2fc81ee339916ca0f509a8111
jefrailey <jcaetof@gmail.com>
2014-07-21 17:10:53 -0700

    Update chesfellows/wsgi.py to use django-configurations

--- chessfellows/chessfellows/wsgi.py
+++ chessfellows/chessfellows/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chessfellows.settings")
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessfellows.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
+from configurations.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
