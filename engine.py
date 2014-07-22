2e71f1a9deaf160ee666423e94ae526041cd32ff
jefrailey <jcaetof@gmail.com>
2014-07-22 14:57:22 -0700

    Add _coord_to_algebraic() and _algebraic_to_coord() to convert positions between (x, y) coords and 'a1' algebraic chess notation

--- engine.py
+++ engine.py
@@ -5,3 +5,13 @@
 #
 # Algebraic notation for a position is:
 # algebraic_pos = chr(x) + chr(y)
+
+
+def _coord_to_algebraic(coord):
+    x, y = coord
+    return chr(x) + chr(y)
+
+
+def _algebraic_to_coord(algebraic):
+    x, y = algebraic[0], algebraic[1]
+    return ord(x), ord(y)
