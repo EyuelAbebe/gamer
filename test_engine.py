18da38e948bf3634fcc4cf49f9ac2645a9c3deaf
jefrailey <jcaetof@gmail.com>
2014-07-22 17:36:29 -0700

    Add test_SimpleUnit_possible_moves_into_ally_space() to Assert moves into allied units are not considered valid.

--- test_engine.py
+++ test_engine.py
@@ -62,3 +62,13 @@ def test_SimpleUnit_possible_moves_empty_space():
     board = EMPTY_BOARD.copy()
     board[(p.x, p.y)] = p
     assert p.possible_moves(board) == [(p.x, p.y + 1)]
+
+
+def test_SimpleUnit_possible_moves_into_ally_space():
+    u"""Assert moves into allied units are not considered valid."""
+    p = engine.SimpleUnit(VALID_COORDS[0], 'white')
+    q = engine.SimpleUnit(VALID_COORDS[1], 'white')
+    board = EMPTY_BOARD.copy()
+    board[(p.x, p.y)] = p
+    board[(q.x, q.y)] = q
+    assert p.possible_moves(board) == []
